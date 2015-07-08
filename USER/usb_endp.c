3d8b1f03071b64dcc5bfd1ae07404fa39a5fe1cd
lucus <lixin9311@gmail.com>
2015-07-08 09:01:07 +0800

    improve the protocol.

--- USER/usb_endp.c
+++ USER/usb_endp.c
@@ -71,15 +71,23 @@ void EP1_IN_Callback(void)
 // +-------+---+---+---+---+---+---+---+---+---------------+---------------+---------------+---------------+
 // | Bits  | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 |               |   Variable    |                               |
 // +-------+---+---+---+---+---+---+---+---+---------------+---------------+-------------------------------+
-// |       | TYPE  |  RSV  |     FRAG      | Packet Length |    Payload    |          FEC(Optional)        |
+// |       | TYPE  |  CMD  |     FRAG      | Packet Length |    Payload    |          FEC(Optional)        |
 // +-------+-------+-----------------------+---------------+---------------+-------------------------------+
 // 1. Frame Type
 //	00 - Set Mac
 //		The payload carries the mac to be set.
 //	01 - Message
 //		The payload carries the raw message to sent, see raw_write().
-//	10 - Reserved
-//	11 - Reserved
+//	10 - Distance / Location poll
+//	11 - Command
+//		CMD - 00
+//			Reboot
+//		CMD - 01
+//			Write Reg
+//		CMD - 10
+//			Read Reg
+//		CMD - 11
+//			Set log level?
 // 2. Packet Length
 //	Total length of all Payloads in a sequence in Unsigned Integer 8.
 // 3. FEC(OPTIONAL)
